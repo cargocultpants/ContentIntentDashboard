c5bf86d2afff19dec1de7573e823acea99a83437
Shannon Matloob <smtlb90@gmail.com>
2017-06-26 16:22:23 -0400

    Initialize express server and add nodemon package

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-// Load HTTP module
-const http = require('http');
+// Dependencies
+const express = require('express');
+const app = express();
 
-//Create HTTP server and listen on port 8000 for requests
-// Set the response HTTP header with HTTP status and Content type
-// Send the response body "Hello World"
-// Print URL for accessing server
+app.get('/', function(req, res) {
+  res.send('Yep.');
+});
 
-http.createServer(
-  function(request, response){
-    response.writeHead(
-      /* HTTP status */,
-      { 'Content-Type': /* Content Type */ }
-    );
-		response.end(/*response body*/);
-	}
-)
-.listen(8000);
-console.log(/*URL for accessing server*/);
+// Declare port variable;
+const port = 8000;
 
-
-http
-	.createServer(function(request, response) {
-		response.writeHead(200, { 'Content-Type': 'text/plain' });
-		response.end('Hello World\n');
-	})
-	.listen(8000);
-console.log('Server running at http://localhost:8000/');
+// Create listener
+app.listen(port, function() {
+  console.log('This is port: ', port);
+});

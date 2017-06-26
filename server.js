613f43ae17aaec07b9d39c162b23edd9ec9c0266
Shannon Matloob <smtlb90@gmail.com>
2017-06-26 18:42:36 -0400

    Add comments to configurations

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require('express');
 const app = express();
 const serveStatic = require('serve-static');
 
-// Application Settings
+
+////////////// Application Settings ////////////
+
+// make express look in the root directory for assets
 app.use(express.static('./'));
+
+// reset ejs render to look in the root folder
 app.set('views', './');
+
+// home page
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
 
-// Declare port variable;
-const port = 8000;
+// process.env.PORT is the variable for Heroku's port
+const port = process.env.PORT || 8000;
 
 // Create listener
 app.listen(port, function() {

46b779e42986e723f17c16363bd98cbc8dc20939
Shannon Matloob <smtlb90@gmail.com>
2017-08-08 13:30:55 -0400

    Open a connection to the ContentIntent database on locally running instance of MongoDB

--- server.js
+++ server.js
@@ -2,7 +2,19 @@
 const express = require('express');
 const app = express();
 const serveStatic = require('serve-static');
+const mongoose = require('mongoose');
 
+// Open a connection to the ContentIntent database on locally running instance of MongoDB
+
+mongoose.connect('mongodb://localhost:27017/contentintent');
+
+// Get notified about connection's success
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to ContentIntent Database');
+});
 
 ////////////// Application Settings ////////////
 

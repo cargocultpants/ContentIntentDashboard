3c09cc5e8572ad2b0d8382b97fdaa282b85aece0
Shannon Matloob <smtlb90@gmail.com>
2017-08-10 14:13:46 -0400

    Add admin status verification for users

--- server.js
+++ server.js
@@ -14,7 +14,50 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Connected to ContentIntent Database');
+
+// Get a reference to and define a schema
+
+// userSchema maps to a MongoDB collection "users" and defines the shape of the documents within that collection.
+
+const userSchema = mongoose.Schema({
+    name: String,
+    admin: Boolean
 });
+
+userSchema.methods.verify = function () {
+  let verify;
+  this.admin ? verify = "approved" : verify = "denied";
+  const msg = `Administrative access has been ${verify} for ${this.name}.`;
+  console.log(msg);
+} //end verify method
+
+const User = mongoose.model('User', userSchema);
+
+const jonah = new User({
+    name: 'Jonah Bliss',
+    admin: true
+});
+console.log(jonah.name);
+
+const shannon = new User({
+    name: 'Shannon Matloob',
+    admin: true
+});
+shannon.verify();
+
+// jonah.save(function (err, shannon) {
+//   if (err) return console.error(err);
+//   console.log(shannon.name, ' has been saved into the database.');
+// }); //save function
+
+User.find(function (err, users) {
+  if (err) return console.error(err);
+  console.log(users.length);
+  console.log("All the users: ", users);
+});
+
+
+}); //end db.once() function
 
 ////////////// Application Settings ////////////
 

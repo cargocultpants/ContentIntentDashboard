45566cc5d213a4a350a64959f6f9a81b71cd6afd
Shannon Matloob <smtlb90@gmail.com>
2017-08-10 17:57:27 -0400

    Set up schema to have nested score schema when it's created

--- contentSchema.js
+++ contentSchema.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
+require('./scoreSchema.js')
 
 // Get a reference to and define a schema
 
 // contentSchema maps to a MongoDB collection "content" and defines the shape of the documents within that collection.
 
 const contentSchema = mongoose.Schema({
-    user: [userSchema],
+    user: String,
     title: String,
     author: String,
     medium: Array,
-    date: Date
+    date: Date,
+    campaign: String,
+    score: [Score.schema]
 });
 
 
-const Content = mongoose.model('Content', contentSchema);
+const content = mongoose.model('Content', contentSchema);
 
 const dummyPost = mongoose.Schema({
     user: [userSchema].name,
@@ -34,5 +37,7 @@ Content.find(function (err, content) {
   console.log(content.length);
   console.log("All the content: ", content);
 });
+
+module.exports = content;
 
 // https://docs.mongodb.com/getting-started/shell/insert/

d271416c2bc0d93b6dbf13d18d6a89e7214ab8b9
Shannon Matloob <smtlb90@gmail.com>
2017-08-10 17:41:06 -0400

    Move user schema code from server file to userSchema file

--- userSchema.js
+++ userSchema.js
@@ -1,15 +1,45 @@
 const mongoose = require('mongoose');
 
-const adminSchema = mongoose.Schema({
-    name: String
+// Get a reference to and define a schema
+
+// userSchema maps to a MongoDB collection "users" and defines the shape of the documents within that collection.
+
+const userSchema = mongoose.Schema({
+    name: String,
+    admin: Boolean
 });
 
-const Admin = mongoose.model('Admin', adminSchema);
+userSchema.methods.verify = function () {
+  let verify;
+  this.admin ? verify = "approved" : verify = "denied";
+  const msg = `Administrative access has been ${verify} for ${this.name}.`;
+  console.log(msg);
+} //end verify method
 
-let admin = new Admin(
-  { name: 'Jonah' }
-);
-console.log(admin.name);
+const User = mongoose.model('User', userSchema);
+
+const jonah = new User({
+    name: 'Jonah Bliss',
+    admin: true
+});
+console.log(jonah.name);
+
+const shannon = new User({
+    name: 'Shannon Matloob',
+    admin: true
+});
+shannon.verify();
+
+// jonah.save(function (err, shannon) {
+//   if (err) return console.error(err);
+//   console.log(shannon.name, ' has been saved into the database.');
+// }); //save function
+
+User.find(function (err, users) {
+  if (err) return console.error(err);
+  console.log(users.length);
+  console.log("All the users: ", users);
+});
 
 
 // https://docs.mongodb.com/getting-started/shell/insert/

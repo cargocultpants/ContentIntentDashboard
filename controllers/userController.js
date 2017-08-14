39a10967083522263290fd03aa45d256f872bf33
Shannon Matloob <smtlb90@gmail.com>
2017-08-14 13:25:27 -0400

    Start users show page

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require('../models/userSchema.js');
 const router = express.Router();
 
-router.get('/users', function(req, res){
+router.get('/', function(req, res){
 	User.find({}, function(err, foundUsers){
 		res.render('users/index.ejs', {
 			users: foundUsers

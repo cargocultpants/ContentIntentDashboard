44f84d40560654a41846f1a82480ae2723b75120
Shannon Matloob <smtlb90@gmail.com>
2017-08-22 12:54:52 -0400

    Save current setting before testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,80 @@
 const express = require('express');
-const User = require('../models/userSchema.js');
 const router = express.Router();
 
+const User = require('../models/userSchema.js');
+
+// Index: GET '/users'
 router.get('/', function(req, res){
-	User.find({}, function(err, foundUsers){
-		res.render('users/index.ejs', {
-			users: foundUsers
+	User.find(function(err, userIndex){
+		err ? console.log(err) : res.render(
+			'users/index.ejs',
+			{ users: userIndex }
+		);
+	});
+});
+
+// Show: GET '/users/_id'
+router.get('/:id', function(req, res) {
+	User.findById(req.params.id, function(err, user) {
+		res.render('users/show.ejs', {
+			users: user
 		});
 	});
 });
+
+
+// New: GET '/users/new'
+// router.#('/#', function(req, res) {
+// 	User.#(req, function(err, obj) {
+// 		res.#('users/#.ejs', {
+// 			user: obj
+// 		});
+// 	});
+// });
+
+// Create: POST '/users'
+// router.#('/#', function(req, res) {
+// 	User.#(req, function(err, obj) {
+// 		res.#('users/#.ejs', {
+// 			user: obj
+// 		});
+// 	});
+// });
+
+// Edit: GET '/users/_id/edit'
+// router.#('/#', function(req, res) {
+// 	User.#(req, function(err, obj) {
+// 		res.#('users/#.ejs', {
+// 			user: obj
+// 		});
+// 	});
+// });
+
+// Update: PUT '/users/_id'
+// router.#('/#', function(req, res) {
+// 	User.#(req, function(err, obj) {
+// 		res.#('users/#.ejs', {
+// 			user: obj
+// 		});
+// 	});
+// });
+
+// Delete: DELETE '/users/_id'
+// router.#('/#', function(req, res) {
+// 	User.#(req, function(err, obj) {
+// 		res.#('users/#.ejs', {
+// 			user: obj
+// 		});
+// 	});
+// });
+
+// Seed: GET '/users/seed'  <-- dev only: route removed before deployment
+// router.#('/#', function(req, res) {
+// 	User.#(req, function(err, obj) {
+// 		res.#('users/#.ejs', {
+// 			user: obj
+// 		});
+// 	});
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,43 +1,45 @@
-// Dependencies
-const express = require('express');
-const app = express();
-// const serveStatic = require('serve-static');
-const mongoose = require('mongoose');
-const uri = 'mongodb://heroku_b7l5h84r:d72i2peoj8gcj8rd65sf9euk6a@ds133340.mlab.com:33340/heroku_b7l5h84r';
+// DEPENDENCIES
+const express = require('express'),
+      mongoose = require('mongoose'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
 
+      uri = 'mongodb://heroku_b7l5h84r:d72i2peoj8gcj8rd65sf9euk6a@ds133340.mlab.com:33340/heroku_b7l5h84r';
 
-////////////// Application Settings ////////////
+// process.env.PORT is the variable for Heroku's port
+const port = process.env.PORT || 8000;
 
-// make express look in the public directory for assets
-app.use(express.static('public'));
-
-// Open a connection to the ContentIntent database on locally running instance of MongoDB
-
-// mongoose.connect('mongodb://localhost:27017/contentintent');
+// DATABASE
 mongoose.connect(uri, { useMongoClient: true });
 
+const db = mongoose.connection;
 
 // Get notified about connection's success
-
-const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Connected to ContentIntent Database');
 }); //end db.once() function
 
-const usersController = require('./controllers/userController.js');
-app.use('/users', usersController);
+// MIDDLEWARE
 
-// home page
+// Make express look in the public directory for assets
+app.use(express.static('public'));
+app.use(methodOverride('_method')); // <-- allow POST, PUT, and DELETE from a form
+app.use(bodyParser.urlencoded({ extended: false })); // <-- req.body
+app.use(bodyParser.json()); // <-- req.body
+
+// ROOT ROUTE
 app.get('/', function(req, res){
 	res.render('index.ejs');
 });
 
-// var contentController = require('./controllers/content.js');
-// app.use('/content', contentController);
+// CONTROLLERS
+const usersController = require('./controllers/userController.js');
+app.use('/users', usersController);
 
-// process.env.PORT is the variable for Heroku's port
-const port = process.env.PORT || 8000;
+// const contentController = require('./controllers/content.js');
+// app.use('/content', contentController);
 
 // Create listener
 app.listen(port, function() {

ceb13e9a4a8057112481d49a6c2f2490252225a8
Shannon Matloob <smtlb90@gmail.com>
2017-11-18 14:29:06 -0500

    import dependencies and middleware

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// import dependencies and path node library
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,14 +6,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+// import modules from routes directory
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// create app object
 var app = express();
 
-// view engine setup
+// set up view template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// add middleware libraries into request handling chain
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,6 +26,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// add Express.static to serve all the static files in the /public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);

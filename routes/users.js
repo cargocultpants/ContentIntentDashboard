dca68e5799d34200679ce7e668f3f11d3999d437
Shannon Matloob <smtlb90@gmail.com>
2017-11-19 17:05:34 -0500

    Add mockup view

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
+// load express module
 var express = require('express');
+
+// use express module to get express.Router object
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Users listing');
+});
+
+/* GET content listing. */
+router.get('/content', function(req, res, next) {
+  res.send('Content listing');
 });
 
 module.exports = router;

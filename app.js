f9419f3967be2d79fdde0789f6968c364daca85b
Shannon Matloob <smtlb90@gmail.com>
2018-02-04 14:48:42 -0800

    Simply app.js and index.js

--- app.js
+++ app.js
@@ -1,69 +1,78 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require("mongodb");
+// const express = require('express');
+// const bodyParser = require('body-parser');
 require('dotenv').config();
+const MongoClient = require("mongodb").MongoClient;
 
 // create app object
-const app = express();
-const USER_COLLECTION = "users";
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// const app = express();
+// const USER_COLLECTION = "users";
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // declare db variable;
-let db;
+// let db;
+
+const displayResults = function(err, result) {
+  if (err) console.log(err);
+  else {
+    console.log(result);
+  }
+}
 
 // initialize database connection
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
-  // initialize db object
-  db = database;
+  // initialize database
   console.log("Database connection ready");
 
-  const port = process.env.PORT || 8080;
+  database.collection('users').find().toArray(displayResults());
+});
 
-    // initialize app
-    app.listen(port, function () {
-      console.log("App now running on port: ", port);
-    });
-  }
-);
+//   const port = process.env.PORT || 8080;
+//
+//     // initialize app
+//     app.listen(port, function () {
+//       console.log("App now running on port: ", port);
+//     });
+//   }
+// );
 
 /////////////
 // ROUTES //
 ////////////
 
 // error handler
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
+// function handleError(res, reason, message, code) {
+//   console.log("ERROR: " + reason);
+//   res.status(code || 500).json({"error": message});
+// }
 
 
 // add Express.static to serve all the static files in the public directory
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // import modules from routes directory
-const index = require('./routes/index');
-const users = require('./routes/users');
-const mockup = require('./routes/mockup');
+// const index = require('./routes/index');
+// const users = require('./routes/users');
+// const mockup = require('./routes/mockup');
 
-const ejs = require('ejs');
+// const ejs = require('ejs');
 
 // set up view template engine
-app.set('views', './views')
-app.set('view engine', 'ejs');
+// app.set('views', './views')
+// app.set('view engine', 'ejs');
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-
-module.exports = app;
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+//
+// module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
+const port = 3000;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ContentIntent' });
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-
-
-module.exports = router;
+app.listen(port, () => {
+  console.log('Server listening on port:  ', port);
+});

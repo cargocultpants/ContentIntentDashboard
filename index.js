20266cff03a7c5d874549de405ce51ddecdc883f
Shannon Matloob <smtlb90@gmail.com>
2018-02-10 22:11:43 -0800

    Fix ports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const port = 3000 || process.env.PORT;
+
+const port = process.env.PORT || 8080;
 // load users router module
 const app = express();
 require('./mongoose.js');
